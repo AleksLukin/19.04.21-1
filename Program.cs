2514f830d9e8bc937e02dd179900de942ba03b87
AleksLukin <75177874+AleksLukin@users.noreply.github.com>
2021-04-19 23:49:39 +0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,14 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _19._04._21_1
+namespace _19._04._21_1//
 {
 
     public static class Program
     {
         public class TestCase
         {
-            public int NUMBER { get; set; }
+            public int NUMBER { get; set; }//
             public int Excpected { get; set; }
             public Exception ExcpectedException { get; set; }
         }
@@ -20,7 +20,7 @@ namespace _19._04._21_1
         {
             try
             {
-                var actual = CheckNumber(testCase.NUMBER);
+                var actual = CheckNumber(testCase.NUMBER);//
 
                 if (actual == testCase.Excpected)
                 {
@@ -28,7 +28,7 @@ namespace _19._04._21_1
                 }
                 else
                 {
-                    Console.WriteLine("недействительный тест");
+                    Console.WriteLine("недействительный тест");//
                 }
             }
 
@@ -36,7 +36,7 @@ namespace _19._04._21_1
             {
                 if (testCase.ExcpectedException != null)
                 {
-                    Console.WriteLine("действительный тест");
+                    Console.WriteLine("действительный тест");//
                 }
                 else
                 {
@@ -46,12 +46,12 @@ namespace _19._04._21_1
 
         }
 
-        static void Main(string[] args)
+        static void Main(string[] args)//
         {
             var testCase1 = new TestCase()
             {
                 NUMBER = 5,
-                Excpected = 5,
+                Excpected = 5,//
 
             };
 
@@ -59,7 +59,7 @@ namespace _19._04._21_1
 
         }
 
-        static int CheckNumber(int number)
+        static int CheckNumber(int number)//
         {
             number = int.Parse(Console.ReadLine());
             int d = 0;
@@ -69,7 +69,7 @@ namespace _19._04._21_1
             {
                 if (number % i == 0)
                 {
-                    d++;
+                    d++;//
                 }
                 else
                 {
@@ -77,7 +77,7 @@ namespace _19._04._21_1
                 }
                 if (d == 0)
                 {
-                    Console.WriteLine("d-простое число");
+                    Console.WriteLine("d-простое число");//
                 }
                 else
                 {
@@ -85,7 +85,7 @@ namespace _19._04._21_1
                 }
                 break;
             }
-            return number;
+            return number;//
         }
     }
 }
